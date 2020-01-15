f358eda6650663ec99f379f595dbcb08d63f775b
Budr <budr245@gmail.com>
2020-01-15 15:21:21 +0200

    add hover and scroll

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 (() => {
     'use strict';
+    //burger
     const burgerItem = document.querySelector('.burger');
     const menu = document.querySelector('.header__nav');
     const menuCloseItem = document.querySelector('.header__nav-close');
@@ -8,5 +9,17 @@
     });
     menuCloseItem.addEventListener('click', () => {
         menu.classList.remove('header__nav_active')
-    })
+    });
+
+
+    //scroll
+    const scrollAbout = document.querySelector('a[href="#about"]');
+    scrollAbout.addEventListener('click', (e) => {
+        e.preventDefault();
+        const about = document.querySelector('#about');
+        about.scrollIntoView({
+            behavior: 'smooth',
+            block: "start"
+        });
+    });
 })();
